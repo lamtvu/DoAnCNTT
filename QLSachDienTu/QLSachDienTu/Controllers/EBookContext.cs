18f1333ead8a70db639fd2efb1c9c552ea6e53f5
unknown <duykhang010500@gmail.com>
2020-11-19 12:45:38 +0700

    update data base

---
+++ QLSachDienTu/QLSachDienTu/Controllers/EBookContext.cs
@@ -0,0 +1,17 @@
+﻿using QLSachDienTu.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace QLSachDienTu.Controllers
+{   
+
+    
+    class EBookContext:DbContext
+    {
+    }
+}
---
+++ QLSachDienTu/QLSachDienTu/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace QLSachDienTu.Models
+{   
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public string username { get; set; }
+        public string office { get; set; }
+        public string mail { get; set; }
+        private string password { get; set; }
+        public byte[] avatar { get; set; }
+
+        public ICollection<Mail> mailSent { get; set; }
+        public ICollection<Mail> mailReceive { get; set; }
+
+        public ICollection<Schedule> schedule { get; set; }
+        public ICollection<Book> bookOwner { get; set; }
+    }
+}
